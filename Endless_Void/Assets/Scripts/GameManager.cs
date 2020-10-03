3233d3fc79e43d801a156bd19ceaf7d36b5be776
FireGamer3 <gm31334@gmail.com>
2020-10-02 19:00:56 -0500

    Cool partical effect

--- Endless_Void/Assets/Scripts/GameManager.cs
+++ Endless_Void/Assets/Scripts/GameManager.cs
@@ -1,7 +1,12 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
+
+    public TMP_Text Score_Text;
+    private int LastUpdateScore = 0;
+    public int score = 0;
 
     public void Awake() {
         if (instance == null) {
@@ -9,6 +14,13 @@ public class GameManager : MonoBehaviour {
         } else if (instance != this) {
             Debug.Log("Game Manager Instance already defined!");
             Destroy(this);
+        }
+    }
+
+    public void Update() {
+        if (score != LastUpdateScore) {
+            Score_Text.text = "Score: " + score;
+            LastUpdateScore = score;
         }
     }
 }
